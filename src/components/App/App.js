d5d8eeb819b98411c1d86111bb3d32ac2ec75ab0
yuittti <kozachenkos@gmail.com>
2018-07-10 22:54:28 +0300

    Add initial config

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Root from '../Root/Root';
+import store from '../../redux';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import history from '../../history';
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store = {store}>
+                <ConnectedRouter history = {history}>
+                    <Root />
+                </ConnectedRouter>
+            </Provider>
+        );
+    }
+}
+
+export default App;
---
+++ src/redux/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
+import history from '../history';
+
+const enhancer = applyMiddleware(routerMiddleware(history), thunk, logger);
+const store = createStore(reducer, enhancer);
+
+export default store;
