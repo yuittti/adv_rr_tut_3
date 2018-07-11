b40c8a2d64b2264c52353c18b7f5c058d190be00
yuittti <kozachenkos@gmail.com>
2018-07-11 18:43:25 +0300

    Add prtected route

---
+++ src/components/common/ProtectedRoute.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { moduleName } from '../../ducks/auth';
+import UnAuthorized from './UnAuthorized';
+
+class ProtectedRoute extends Component {
+    render() { 
+        const {component, ...rest} = this.props;
+        return <Route {...rest} render={this.renderProtected} />
+    }
+
+    renderProtected = (routeParams) => {
+        const {component: ProtectedComponent, authorized} = this.props;
+        return authorized ? <ProtectedComponent {...routeParams} /> : <UnAuthorized />
+    }
+}
+ 
+export default connect(state => ({
+    authorized: !!state[moduleName].user
+}))(ProtectedRoute);
---
+++ src/components/common/UnAuthorized.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class UnAuthorized extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Unauthorized! please <Link to='/auth/signin'>Sign In</Link></h1>
+            </div>
+        );
+    }
+}
+ 
+export default UnAuthorized;
