586226a88640dd9c49e7597c314a1d844a2c81d7
yuittti <kozachenkos@gmail.com>
2018-07-13 19:31:17 +0300

    Add Auth saga

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase'
 import { appName } from '../config';
 import { Record } from 'immutable';
+import { all, call, put, take } from 'redux-saga/effects';
 
 const ReducerRecord = Record({
     user: null,
@@ -44,24 +45,60 @@ export default function reducer(state = new ReducerRecord(), action) {
 }
 
 export function signUp(email, password) {
-    
-    return (dispatch) => {
-        dispatch({
-            type: SIGN_UP_REQUEST
-        });
-
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(user => dispatch({
-                type: SIGN_UP_SUCCESS,
-                payload: {user}
-            }))
-            .catch(error => dispatch({
-                type: SIGN_UP_ERROR,
-                error
-            }));
-        
+    return {
+        type: SIGN_UP_REQUEST,
+        payload: {email, password}
     }
 }
+
+export const signUpSaga = function * () {
+    const auth = firebase.auth();
+
+    while(true) {
+        const action = yield take(SIGN_UP_REQUEST);
+
+        try {
+            const user = yield call(
+                [auth, auth.createUserWithEmailAndPassword], 
+                action.payload.email, 
+                action.payload.password
+            );
+
+            yield put({
+                type: SIGN_UP_SUCCESS,
+                payload: {user}
+            });
+
+        } catch(error) {
+            yield put({
+                type: SIGN_UP_ERROR,
+                error
+            })
+        }
+    }
+}
+
+// AC with redux-thunk
+// ---
+// export function signUp(email, password) {
+    
+//     return (dispatch) => {
+//         dispatch({
+//             type: SIGN_UP_REQUEST
+//         });
+
+//         firebase.auth().createUserWithEmailAndPassword(email, password)
+//             .then(user => dispatch({
+//                 type: SIGN_UP_SUCCESS,
+//                 payload: {user}
+//             }))
+//             .catch(error => dispatch({
+//                 type: SIGN_UP_ERROR,
+//                 error
+//             }));
+        
+//     }
+// }
 
 firebase.auth().onAuthStateChanged(user => {
     // tmp solution
@@ -71,3 +108,9 @@ firebase.auth().onAuthStateChanged(user => {
         payload: {user}
     })
 });
+
+export const saga = function * () {
+    yield all([
+        signUpSaga()
+    ])
+}
