725e510ccecee031916109bc8f44bbfa78fe2a0b
yuittti <kozachenkos@gmail.com>
2018-07-15 23:56:41 +0300

    Add fetching Events

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import AdminPage from '../routes/AdminPage';
 import PersonPage from '../routes/PersonPage';
+import EventsPage from '../routes/EventsPage';
 import AuthPage from '../routes/AuthPage';
 import ProtectedRoute from '../common/ProtectedRoute';
 import { connect } from 'react-redux';
@@ -19,6 +20,7 @@ class Root extends Component {
                 {btn}
                 <ProtectedRoute path = '/admin' component = {AdminPage} />
                 <ProtectedRoute path = '/people' component = {PersonPage} />
+                <ProtectedRoute path = '/events' component = {EventsPage} />
                 <Route path = '/auth' component = {AuthPage} />
             </div>
         );
---
+++ src/components/routes/EventsPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import EventList from '../../components/EventList/EventList';
+
+class EventsPage extends Component {
+
+    render() { 
+        return (
+            <div>
+                <h1>Events Page</h1>
+                <EventList />
+            </div>
+        );
+    }
+}
+ 
+export default EventsPage;
