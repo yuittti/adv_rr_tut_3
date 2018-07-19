bcd3fb5d558e65da779dd54f724007a95fd77bb1
yuittti <kozachenkos@gmail.com>
2018-07-19 18:49:28 +0300

    Add animation for adding people

--- src/components/PeopleTable/PeopleTable.js
+++ src/components/PeopleTable/PeopleTable.js
@@ -3,8 +3,17 @@ import { connect } from 'react-redux';
 import { moduleName, fetchAllPeople, peopleListSelector } from '../../ducks/people';
 import { Table, Column } from 'react-virtualized';
 import 'react-virtualized/styles.css';
+import { TransitionMotion, spring } from 'react-motion';
 
 export class PeopleTable extends Component {
+
+    componentDidUpdate({people}) {
+        if (people.length && this.props.people.length > people.length) {
+            setTimeout(() => {
+                this.table.scrollToRow(this.props.people.length)
+            }, 0);
+        }
+    }
 
     componentDidMount() {
         this.props.fetchAllPeople && this.props.fetchAllPeople();
@@ -12,31 +21,55 @@ export class PeopleTable extends Component {
     
     render() { 
         const {people} = this.props;
+        if (!people.length) return null;
         return (
-            <Table
-                rowCount={people.length}
-                rowGetter={this.rowGetter}
-                rowHeight={40}
-                headerHeight={50}
-                overscanRowCount={2}
-                width={750}
-                height={300}
-                rowClassName="test--people-list__row"
+            <TransitionMotion
+                willEnter={this.willEnter}
+                styles={this.getStyles}
             >
-                <Column
-                    label="firstName"
-                    dataKey="firstName"
-                    width={250} />
-                <Column
-                    label="lastName"
-                    dataKey="lastName"
-                    width={250} />
-                <Column label="email"
-                    dataKey="email"
-                    width={250} />
-            </Table>
+                {interpolatedStyles => 
+                    <Table
+                        rowCount={interpolatedStyles.length}
+                        rowGetter={this.rowGetter}
+                        rowHeight={40}
+                        headerHeight={50}
+                        overscanRowCount={2}
+                        width={750}
+                        height={300}
+                        rowClassName="test--people-list__row"
+                        rowStyle={({index}) => index < 0 ? {} : interpolatedStyles[index].style}
+                        ref={this.setListRef}
+                    >
+                        <Column
+                            label="firstName"
+                            dataKey="firstName"
+                            width={250} />
+                        <Column
+                            label="lastName"
+                            dataKey="lastName"
+                            width={250} />
+                        <Column label="email"
+                            dataKey="email"
+                            width={250} />
+                    </Table>
+                }
+            </TransitionMotion>
         );
     }
+
+    willEnter = () => ({
+        opacity: 0
+    });
+
+    getStyles = () => this.props.people.map(person => ({
+        key: person.uid,
+        style: {
+            opacity: spring(1, {stiffness: 50})
+        },
+        data: person
+    }));
+
+    setListRef = ref => this.table = ref;
 
     rowGetter = ({index}) => {
         return this.props.people[index];
